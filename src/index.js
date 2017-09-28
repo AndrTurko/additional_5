567eefb9fb8d9e2af8bb324b1df82e16747872ab
Andrey Turko <andrturko@gmail.com>
2017-09-28 16:27:25 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+	let pull = [];
+	for (let i = 0; i < config.length; i++) {
+		for (let j = 0; j < str.length; j++) {
+			if (str[j] === config[i][1]) {
+				pull.pop();
+				continue;
+			}
+			if (str[j] === config[i][0]) {
+				pull.push(str[j]);
+				continue;
+			}
+		}
+	}
+	if (pull[0] == undefined) return true;
+	else return false;
 }
